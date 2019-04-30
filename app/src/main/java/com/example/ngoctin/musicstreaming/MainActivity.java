4adf72fc50a5422e6958127b5b037a4048ecc18e
tran ngoc tin <ngoctint1lvc@gmail.com>
2019-04-30 15:26:56 +0700

    create project

---
+++ app/src/main/java/com/example/ngoctin/musicstreaming/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.ngoctin.musicstreaming;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+    private ViewPager viewPager;
+    private TabLayout tabLayout = null;
+    public static String STR_FRIEND_FRAGMENT = "FRIEND";
+    public static String STR_GROUP_FRAGMENT = "GROUP";
+    public static String STR_INFO_FRAGMENT = "INFO";
+
+    private FloatingActionButton floatButton;
+//    private ViewPagerAdapter adapter;
+
+//    private FirebaseAuth mAuth;
+//    private FirebaseAuth.AuthStateListener mAuthListener;
+//    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("Music Streaming");
+        }
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        floatButton = (FloatingActionButton) findViewById(R.id.fab);
+        initTab();
+    }
+
+    private void initTab() {
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorIndivateTab));
+//        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+//        setupTabIcons();
+    }
+
+//    private void setupTabIcons() {
+//        int[] tabIcons = {
+//                R.drawable.ic_tab_person,
+//                R.drawable.ic_tab_group,
+//                R.drawable.ic_tab_infor
+//        };
+//
+//        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+//        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+//        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+//    }
+
+//    private void setupViewPager(ViewPager viewPager) {
+//        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        adapter.addFrag(new FriendsFragment(), STR_FRIEND_FRAGMENT);
+//        adapter.addFrag(new GroupFragment(), STR_GROUP_FRAGMENT);
+//        adapter.addFrag(new UserProfileFragment(), STR_INFO_FRAGMENT);
+//        floatButton.setOnClickListener(((FriendsFragment) adapter.getItem(0)).onClickFloatButton.getInstance(this));
+//        viewPager.setAdapter(adapter);
+//        viewPager.setOffscreenPageLimit(3);
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                ServiceUtils.stopServiceFriendChat(MainActivity.this.getApplicationContext(), false);
+//                if (adapter.getItem(position) instanceof FriendsFragment) {
+//                    floatButton.setVisibility(View.VISIBLE);
+//                    floatButton.setOnClickListener(((FriendsFragment) adapter.getItem(position)).onClickFloatButton.getInstance(MainActivity.this));
+//                    floatButton.setImageResource(R.drawable.plus);
+//                } else if (adapter.getItem(position) instanceof GroupFragment) {
+//                    floatButton.setVisibility(View.VISIBLE);
+//                    floatButton.setOnClickListener(((GroupFragment) adapter.getItem(position)).onClickFloatButton.getInstance(MainActivity.this));
+//                    floatButton.setImageResource(R.drawable.ic_float_add_group);
+//                } else {
+//                    floatButton.setVisibility(View.GONE);
+//                }
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
+//    }
+
+//    class ViewPagerAdapter extends FragmentPagerAdapter {
+//        private final List<Fragment> mFragmentList = new ArrayList<>();
+//        private final List<String> mFragmentTitleList = new ArrayList<>();
+//
+//        public ViewPagerAdapter(FragmentManager manager) {
+//            super(manager);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            return mFragmentList.get(position);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return mFragmentList.size();
+//        }
+//
+//        public void addFrag(Fragment fragment, String title) {
+//            mFragmentList.add(fragment);
+//            mFragmentTitleList.add(title);
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//
+//            // return null to display only the icon
+//            return null;
+//        }
+//    }
+}
